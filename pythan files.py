2ba5224dce5e38fcef8bbf4ac8454cf2b0c24db4
91001480 <sairameshkkl@gmail.com>
2024-08-15 13:50:38 +0530

    first commit

---
+++ pythan files.py
@@ -0,0 +1,15 @@
+class sundayclass:
+    subj='python'
+    time='10:00'
+    def __init__(self,name,mno,aadhar,gender):
+        self.name=name
+        self.mno=mno
+        self.aadhar=aadhar
+        self.gender=gender
+
+obj1=sundayclass('sai',9100148001,613188241222,'male')
+obj2=sundayclass('ramesh',100036743,144222555,'male')
+obj3=sundayclass('nandini',1234567,123456789012,'female')
+print(obj3.name)
+
+        
